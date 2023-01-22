eeba0a64a12c9c4c8faf762757283ee92e642a85
Oumar <oumarly.consultant@gmail.com>
2023-01-21 22:11:42 -0500

    add

--- Connect4Game.js
+++ Connect4Game.js
@@ -4,7 +4,25 @@ const p2 = prompt("Player 2 enter your name, you will be red");
 
 $('h3').text(p1+": it is your turn, please pick a column to drop your blue chip.");
 
-//function to change cell color after picking column
-//grab all cells
-let cells = $('td');
+//function turnBlue
+function turnBlue(){
+    this.toggleClass("turnBlue");
+}
+//function turnRed
+function turnRed(){
+    this.toggleClass("turnRed");
+}
 
+$('#cell')
+
+let who_play = 1;
+
+//change color of last chip based on who plays
+function play(l,k){
+    if (k % 2 == 0){
+        this.toggleClass("turnRed");
+
+    }else{
+        this.toggleClass("turnBlue");
+    }
+}
