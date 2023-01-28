b2efe74487f83abc17bc3ded7795c03deda7868c
Oumar <oumarly.consultant@gmail.com>
2023-01-28 13:42:57 -0500

    Jan 28

---
+++ Connect4Game/Connect4Game.js
@@ -0,0 +1,15 @@
+/* Instructions 
+1.Create the required functions
+
+a. function that return the row and column index of a tag that was clicked
+b. function that, given a row and column index of a tag, return the color of that tag
+c. function that, given a row and column index of a tag, return the index and column of the bottom most tag non blue/red
+d. function that, given a row and column index of a tag, change color of that tag
+e. function that, given a row and column index, check for the horizontal win
+d. function that, given a row and column index, check for the vertical win
+f. function that, given a row and column index, check for the diagonal win
+
+2.Create game dynamic
+a. get players color and name
+b. go through 
+*/
