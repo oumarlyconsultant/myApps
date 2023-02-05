1a5f482e4d65684b6fc4279b7d28d1c0ee887dee
oumarlyconsultant <oumarly.consultant@gmail.com>
2023-02-05 15:15:16 -0500

    worked on blog_project

--- blog_project/mysite/blog/forms.py
+++ blog_project/mysite/blog/forms.py
@@ -1,8 +1,6 @@
 from django import forms
 from blog.models import Post, Comment
 
-
-
 class PostForm(forms.ModelForm):
     class Meta():
         model = Post
--- blog_project/mysite/blog/views.py
+++ blog_project/mysite/blog/views.py
@@ -1,8 +1,65 @@
-from django.shortcuts import render
-from django.views.generic import (TemplateView)
+from django.shortcuts import render, get_object_or_404,redirect
+from django.utils import timezone
+from blog.models import Post,Comment
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixings import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
+from blog.forms import PostForm,CommentForm
+
 
 # Create your views here.
 class AboutView(TemplateView):
     template_name = 'about.html'
 
+class PostListView(ListView):
+    model = Post
 
+    def get_queryset(self):
+        return Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date') #See Field lookups in documentation... this is similar to an SQL query
+
+class PostDetailView(DetailView):
+    model = Post
+
+class CreatePostView(LoginRequiredMixin,CreateView):
+    login_url = '/login/'
+    redirect_field_name = '/blog/post_detail.html'
+    form_class = PostForm
+    model = Post
+
+class PostUpdateView(LoginRequiredMixin,UpdateView):
+    login_url = '/login/'
+    redirect_field_name = '/blog/post_detail.html'
+    form_class = PostForm
+    model = Post   
+
+#Delete post
+class PostDeleteView(LoginRequiredMixin,DeleteView):
+    model = Post
+    success_url = reverse_lazy('post_list')
+
+class DraftListView(LoginRequiredMixin,ListView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_list.html'
+    model = Post
+    def get_queryset(self):
+        return Post.objects.filter(published_date__isnull=True).order_by('created_date') #See Field lookups in documentation... this is similar to an SQL query
+
+
+########################################
+########################################
+
+@login_required
+def add_comment_to_post(request,pk):
+    post = get_object_or_404(Post,pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail',pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request,'blog/comment_form.html',{'form':form})
+        
