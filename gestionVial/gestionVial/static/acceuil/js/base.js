8034d992eda58e6daac736b5999f59bc0742540d
Oumar <oumarly.consultant@gmail.com>
2023-02-17 21:59:12 -0500

    updated Feb 7

--- gestionVial/gestionVial/static/acceuil/js/base.js
+++ gestionVial/gestionVial/static/acceuil/js/base.js
@@ -1,18 +1,19 @@
 $(document).ready(function() {  
      $('.nav-link-collapse').on('click', function() {  
        $('.nav-link-collapse').not(this).removeClass('nav-link-show');  
-       $(this).toggleClass('nav-link-show');  
+       $(this).toggleClass('nav-link-show');
      });
 
-     function runOp(element,urlText){
-      $(element).on('click',function(){
-        // console.log('clicked!');
-        document.location.href = urlText
-       })  
-       
-     }
 
-     runOp('#repEmp',"gP_repEmp/")
+    //  function runOp(element,urlText){
+    //   $(element).on('click',function(){
+    //     // console.log('clicked!');
+    //     document.location.href = urlText
+    //    })  
+       
+    //  }
+
+    //  runOp('#repEmp',"gP_repEmp/")
      
     //  $('#repEmp').on('click',function(){
     //   console.log('clicked!');
