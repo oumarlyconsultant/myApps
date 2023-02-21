c7f95de4d4af7006c15b871f05b7632b98caa883
Oumar <oumarly.consultant@gmail.com>
2023-02-20 20:39:54 -0500

    try postgresql

--- gestionVial/gestionVial/gestionStock/admin.py
+++ gestionVial/gestionVial/gestionStock/admin.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
-from .models import CharacMat,Couleur,Fournisseur,Materiel,CharacProd,Dimension,Produit,Nomenclature
+from .models import ClasseMat,CategorieMat,Couleur,Fournisseur,Materiel,ClasseProd,CategorieProd,Dimension,Produit,Nomenclature
 
 # Register your models here.
-admin.site.register(CharacMat)
+admin.site.register(ClasseMat)
+admin.site.register(CategorieMat)
 admin.site.register(Couleur)
 admin.site.register(Fournisseur)
 admin.site.register(Materiel)
-admin.site.register(CharacProd)
+admin.site.register(ClasseProd)
+admin.site.register(CategorieProd)
 admin.site.register(Dimension)
 admin.site.register(Produit)
 admin.site.register(Nomenclature)
--- gestionVial/gestionVial/gestionStock/models.py
+++ gestionVial/gestionVial/gestionStock/models.py
@@ -2,10 +2,17 @@ from django.db import models
 
 # Create your models here.
 #Materiel brut
-class CharacMat(models.Model):
+class ClasseMat(models.Model):
     classe = models.CharField(max_length=50,blank=False,null=False)
+    info = models.CharField(max_length=200,blank=True,null=True)
+
+    def __str__(self):
+        return self.classe
+
+class CategorieMat(models.Model):
     categorie = models.CharField(max_length=50,blank=False,null=False)
     info = models.CharField(max_length=200,blank=True,null=True)
+    classe = models.ForeignKey(ClasseMat,on_delete=models.CASCADE,blank=True,null=True)
 
     def __str__(self):
         return self.classe+" "+self.categorie
@@ -24,40 +31,47 @@ class Fournisseur(models.Model):
     telephone = models.CharField(max_length=50,blank=True,null=True)
     pays = models.CharField(max_length=50,blank=True,null=True)
     personneRelation = models.CharField(max_length=100,blank=True,null=True)
-    fournisseurFabriquant = models.CharField(max_length=1,choices=(('N','Non'),('O','Oui')),default='N')
+    fournisseurFabriquant = models.BooleanField(default=False)
 
     def __str__(self):
         return self.nom
 
 class Materiel(models.Model):
     reference = models.CharField(max_length=50,blank=True,null=True)
-    designation = models.CharField(max_length=100,blank=False,null=False)
-    uniteDeMesure = models.CharField(max_length=50)
-    uniteDeQuantification = models.CharField(max_length=50,blank=True,null=True)
+    designation = models.CharField(max_length=100,blank=True,null=True)
+    uniteDeMesure = models.CharField(max_length=50,choices=(('m','metre'),('m2','mettre carre'),('m3','mettre cube'),('n/a','pas applicable')),default='m')
+    uniteDeQuantification = models.CharField(max_length=50,choices=(('u','unite'),('m','metre'),('m2','mettre carre'),('m3','mettre cube'),('n/a','pas applicable')),default='u')
     cout = models.FloatField(blank=False,null=False,default=10000)
     prix = models.FloatField(blank=False,null=False,default=17000)
     fabriquant = models.CharField(max_length=100,blank=True,null=True)
-    autresInfo = models.CharField(max_length=200,blank=True,null=True)
+    info = models.CharField(max_length=200,blank=True,null=True)
 
-    characteristiques = models.ForeignKey(CharacMat,on_delete=models.RESTRICT)
-    couleur = models.ForeignKey(Couleur,on_delete=models.RESTRICT)
+    characteristiques = models.ForeignKey(CategorieMat,on_delete=models.CASCADE,blank=True,null=True)
+    couleur = models.ForeignKey(Couleur,on_delete=models.RESTRICT,blank=True,null=True)
     fournisseur = models.ForeignKey(Fournisseur,on_delete=models.RESTRICT,blank=True,null=True)
 
     def __str__(self):
-        return self.designation
+        return self.designation+" "+self.characteristiques+" "+self.couleur
 
 #Produit Fini
-class CharacProd(models.Model):
+class ClasseProd(models.Model):
     classe = models.CharField(max_length=50,blank=False,null=False)
+    info = models.CharField(max_length=200,blank=True,null=True)
+
+    def __str__(self):
+        return self.classe
+
+class CategorieProd(models.Model):
     categorie = models.CharField(max_length=50,blank=False,null=False)
     info = models.CharField(max_length=200,blank=True,null=True)
+    classe = models.ForeignKey(ClasseProd,on_delete=models.CASCADE,blank=True,null=True)
 
     def __str__(self):
         return self.classe+" "+self.categorie
 
 class Dimension(models.Model):
-    largeur = models.IntegerField(blank=True,null=True)
-    hauteur = models.IntegerField(blank=True,null=True)
+    largeur = models.IntegerField(blank=False,null=False,default=0)
+    hauteur = models.IntegerField(blank=False,null=False,default=0)
     profondeur = models.IntegerField(blank=True,null=True)
 
     def __str__(self):
@@ -65,22 +79,22 @@ class Dimension(models.Model):
 
 class Produit(models.Model):
     reference = models.CharField(max_length=50,blank=True,null=True)
-    # designation = models.CharField(max_length=100,blank=False,null=False)
-    uniteDeMesure = models.CharField(max_length=50,blank=True,null=True)
-    uniteDeQuantification = models.CharField(max_length=50,blank=True,null=True)
+    designation = models.CharField(max_length=100,blank=True,null=True)
+    uniteDeMesure = models.CharField(max_length=50,choices=(('m','metre'),('m2','mettre carre'),('m3','mettre cube'),('n/a','pas applicable')),default='m')
+    uniteDeQuantification = models.CharField(max_length=50,choices=(('u','unite'),('m','metre'),('m2','mettre carre'),('m3','mettre cube'),('n/a','pas applicable')),default='u')
     cout = models.FloatField(blank=False,null=False,default=10000)
     prix = models.FloatField(blank=False,null=False,default=17000)
-    autresInfo = models.CharField(max_length=200,blank=True,null=True)
+    info = models.CharField(max_length=200,blank=True,null=True)
 
-    characteristiques = models.ForeignKey(CharacProd,on_delete=models.RESTRICT)
+    characteristiques = models.ForeignKey(CategorieProd,on_delete=models.CASCADE,blank=True,null=True)
     dimension = models.ForeignKey(Dimension,on_delete=models.RESTRICT,blank=True,null=True)
     couleur = models.ForeignKey(Couleur,on_delete=models.RESTRICT,blank=True,null=True)
     materiel = models.ManyToManyField(Materiel,through='Nomenclature')
     
     def __str__(self):
-        return self.characteristiques.classe+" "+self.characteristiques.categorie+" "+self.dimension+" "+self.reference
+        return self.designation+" "+self.characteristiques+" "+self.dimension+" "+self.couleur
 
 class Nomenclature(models.Model):
-    materiel = models.ForeignKey(Materiel,on_delete=models.RESTRICT)
-    produit = models.ForeignKey(Produit,on_delete=models.RESTRICT)
+    materiel = models.ForeignKey(Materiel,on_delete=models.CASCADE)
+    produit = models.ForeignKey(Produit,on_delete=models.CASCADE)
     quantiteMateriel = models.FloatField(blank=False,null=False,default=1)
