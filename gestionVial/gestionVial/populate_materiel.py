546b124aa4e210b5f99dd92d0254cc79c8fef3e0
Oumar <oumarly.consultant@gmail.com>
2023-03-11 00:32:16 -0500

    reinit March 11 Linux

--- gestionVial/gestionVial/populate_materiel.py
+++ gestionVial/gestionVial/populate_materiel.py
@@ -13,6 +13,7 @@ from gestionStock.models import ClasseMat, CategorieMat, Couleur, Materiel
 #Pays OK
 
 import pandas as pd
+import numpy as np
 
 # def populate_classeMat(file_name):
 #     df = pd.read_csv(file_name,sep=",",header=0)
