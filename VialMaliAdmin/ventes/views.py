a0463ad46191c287992b49c0c568c06ac1c64df5
Oumar <oumarly.consultant@gmail.com>
2023-04-02 16:04:48 -0400

    updated linux April 2

--- VialMaliAdmin/ventes/views.py
+++ VialMaliAdmin/ventes/views.py
@@ -5,6 +5,7 @@ from django.http import Http404
 from django.urls import reverse, reverse_lazy
 from django.views.generic import View, ListView, DetailView, CreateView, UpdateView, TemplateView, FormView, DeleteView
 from . import models, forms
+from django.contrib.messages.views import SuccessMessageMixin
 # from . import forms
 # from . import models
 
@@ -31,14 +32,19 @@ class clientsDetails(DetailView):
         context = super().get_context_data(**kwargs)
         return context
     
-class clientsAjouter(CreateView):
+class clientsAjouter(SuccessMessageMixin,CreateView):
     model = models.Client
     template_name = "ventes_clients_ajouter.html"
     fields = ('prenom','nom','nomEntreprise','adresse','telephone','email','info','ville','pays')
+    success_message = "Nouveau profile client cree"
+    success_url = reverse_lazy('ventes:clientsRepertoire')
+
+    def get_success_message(self, cleaned_data):
+        return "Nouveau profile client cree avec succes!"
 
     def post(self,request,*args,**kwargs):
         if "annuler" in request.POST:
-            return HttpResponseRedirect(reverse('ventes:clientsRepertoire'))
+            return HttpResponseRedirect(reverse('ventes:clientsIndex'))
         else:
             return super(clientsAjouter,self).post(request,*args,**kwargs)
     
