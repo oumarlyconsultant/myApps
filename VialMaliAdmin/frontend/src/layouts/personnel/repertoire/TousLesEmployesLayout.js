6ccd13a0417f79a7f62d33b08a013413984e68de
oumarlyconsultant <oumarly.consultant@gmail.com>
2023-06-18 12:09:24 -0400

    June 18 12:09 Wind

--- VialMaliAdmin/frontend/src/layouts/personnel/repertoire/TousLesEmployesLayout.js
+++ VialMaliAdmin/frontend/src/layouts/personnel/repertoire/TousLesEmployesLayout.js
@@ -1,19 +1,49 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState, useMemo } from "react"
 import { Link, NavLink, Outlet, useLoaderData } from "react-router-dom"
-import EmployeListe from "../../../components/personnel/EmployeListe"
+// import EmployeListe from "../../../components/personnel/EmployeListe"
+import Table from "../../../components/Table"
 
 export default function TousLesEmployesLayout(){
 
     const employes = useLoaderData()
+
+    const columns = useMemo(
+        () => [
+            {
+                Header: "Liste des employes",
+                columns: [
+                    {
+                        Header: "Prenom",
+                        accessor: "prenom"
+                    },
+                    {
+                        Header: "Nom",
+                        accessor: "nom"
+                    },
+                    {
+                        Header: "Poste",
+                        accessor: "poste"
+                    },
+                    {
+                        Header: "Departement",
+                        accessor: "departement"
+                    }
+                ]
+            }
+        ],[]
+    )
+    // console.log(employes)
+    console.log(columns)
         
     return(
         <div className="touslesemployes">
         <div className="w3-cell" style={{width:"100%"}}>
-            <h4>Liste de tous les employes actifs</h4>
-            {employes.map(employe => (<div><EmployeListe prenom={employe.prenom} nom={employe.nom} poste={employe.poste} departement={employe.departement} numeroEmploye={employe.numeroEmploye}/><br/></div>))}
+            <Table columns={columns} data={employes}/>
+            {/* <h4>Liste de tous les employes actifs</h4>
+            {employes.map(employe => (<div><EmployeListe prenom={employe.prenom} nom={employe.nom} poste={employe.poste} departement={employe.departement} numeroEmploye={employe.numeroEmploye}/><br/></div>))} */}
         </div>
         <div className="w3-container w3-cell" style={{width:"60%"}}>
-        <Outlet />
+        <Outlet /> 
         </div>
          </div>
     )
